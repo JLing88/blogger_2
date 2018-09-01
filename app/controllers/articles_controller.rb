9e947b44b16f2e8b43e07f4ffb160dc4d54f401a
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-09-01 12:50:01 -0600

    Adds edit view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,31 +1,46 @@
 class ArticlesController < ApplicationController
- def index
+
+  before_action :set_article, only: [:destroy, :edit, :update, :show]
+
+  def index
    @articles = Article.all
- end
+  end
 
- def show
-   @article = Article.find(params[:id])
- end
+  def show
+  end
 
- def new
+  def new
    @article = Article.new
- end
+  end
 
- def create
+  def create
    @article = Article.new(article_params)
    @article.save
 
    redirect_to article_path(@article.id)
- end
+  end
 
- def destroy
-   Article.destroy(params[:id])
-
+  def destroy
+   @article.destroy
    redirect_to articles_path
- end
-end
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
+  end
 
 private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:title, :body)
   end
+end
--- spec/features/user_can_delete_an_article_spec.rb
+++ spec/features/user_can_delete_an_article_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 describe 'user deletes an article' do
   describe 'they link from the show page' do
     it 'displays all articles without the deleted article' do
-      article_1 = Article.create!(title: "Title 1", body: "Body 1")
+      article_3 = Article.create!(title: "Title 3", body: "Body 3")
       article_2 = Article.create!(title: "Title 2", body: "Body 2")
 
-      visit article_path(article_1)
-
+      visit article_path(article_3)
       click_link "Delete"
 
       expect(current_path).to eq(articles_path)
+
       expect(page).to have_content(article_2.title)
-      expect(page).to_not have_content(article_1.title)
+      expect(page).to_not have_content(article_3.title)
     end
   end
 end
